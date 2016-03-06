cceaa547cbce64eafb6a9e116504511dd28b427e
Seoksoon Jang <jss@koino.net>
2016-03-07 00:04:18 +0900

    the first deployment

---
+++ ALTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALTest
+//
+//  Created by SeoksoonJang on 3/6/16.
+//  Copyright © 2016 SeoksoonJang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
